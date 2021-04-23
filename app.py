6e7d86a9e37d6cd12bcceb037740007da282b31e
xue87 <s18313109@stu.edu.tw>
2021-04-23 11:56:04 +0800

    debug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from route import Index,Sendingjpg,PhotoList,Status,Gif
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/plant'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.create_all()
-api.add_resource(Index),'/')
+api.add_resource(Index,'/')
 api.add_resource(Sendingjpg,'/api/photos/<path:path>')
 api.add_resource(PhotoList,'/api/photos/list')
 api.add_resource(Status,'/api/status')
